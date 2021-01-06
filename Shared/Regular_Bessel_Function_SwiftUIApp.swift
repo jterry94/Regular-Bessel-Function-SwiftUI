b3933035fbe4e6bcaa96c6c99b113ee6d3f7eb01
Jeff Terry <terryj@iit.edu>
2021-01-05 19:08:23 -0600

    Initial Commit

---
+++ Shared/Regular_Bessel_Function_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regular_Bessel_Function_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Regular_Bessel_Function_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
